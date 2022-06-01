d475644dc8ed3e569cc3224e62120704f839c351
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2022-05-31 21:23:35 -0400

    e

--- server.js
+++ server.js
@@ -33,12 +33,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 
-    app.get("/", (req, res) => {
-
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-
-   });
-
 // Error / success
 
   
